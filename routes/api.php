0e475f78c3545c768c54f4fbdb015165b8a85352
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2023-09-14 13:00:40 -0300

    Commit 14/09

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ServicoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('store', [ServicoController::class, 'store']);
+
+Route::get('find/descricao/{descricao}',[ServicoController::class,'pesquisarPorDescricao']);
+
+Route::get ('all',
+[ServicoController::class, 'retornarTodos']);
+
+Route::post ('nome', [ServicoController::class, 'pesquisarPorNome']);
+
+Route::delete('delete/{id}',[ServicoController::class, 'excluir']);
+
+Route::put('update', [ServicoController::class, 'update']);
