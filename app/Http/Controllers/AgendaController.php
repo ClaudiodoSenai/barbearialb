f2a3e63b2fb4a833ef983e2ef544b9b3d6db8ea2
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2023-11-14 14:07:35 -0300

    agendamento

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -8,41 +8,21 @@ use Illuminate\Http\Request;
 
 class AgendaController extends Controller
 {
-    public function Funcionariosstore(AgendaFormRequest $request)
+    public function criarHorarioProfissional(AgendaFormRequest $request)
     {
-        $agenda = Agenda::create([
+        $agendaProfissional = Agenda::create([
             'profissional_id' =>  $request->profissional_id,
-            'cliente_id' => $request->cliente_id,
-            'servico_id' => $request->servico_id,
-            'data_hora' => $request->data_hora,
-            'tipo_pagamento' => $request->tipo_pagamento,
-            'valor' => $request->valor
+            'data_hora' => $request->data_hora
 
         ]);
         return response()->json([
             "success" => true,
             "message" => "Agendado com sucesso",
-            "data" => $agenda
+            "data" => $agendaProfissional
         ], 200);
     }
 
-    public function Clientestore(AgendaFormRequest $request)
-    {
-        $agenda = Agenda::create([
-            'profissional_id' =>  $request->profissional_id,
-            'cliente_id' => $request->cliente_id,
-            'servico_id' => $request->servico_id,
-            'data_hora' => $request->data_hora,
-            'tipo_pagamento' => $request->tipo_pagamento,
-            'valor' => $request->valor
 
-        ]);
-        return response()->json([
-            "success" => true,
-            "message" => "Agendado com sucesso",
-            "data" => $agenda
-        ], 200);
-    }
 
     public function retornarTodos()
     {
@@ -54,3 +34,4 @@ class AgendaController extends Controller
     }
 
 }
+tyuio
